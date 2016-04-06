07a04ee5cc9811c3b8c8e3bafb00740e20d995a7
sortona <sortona@en4119640m.cidse.dhcp.asu.edu>
2016-04-05 20:22:05 -0700

    sparql executor endpoint

--- src/main/java/asu/edu/neg_rule_miner/configuration/ConfigurationFacility.java
+++ src/main/java/asu/edu/neg_rule_miner/configuration/ConfigurationFacility.java
@@ -1,20 +1,27 @@
 package asu.edu.neg_rule_miner.configuration;
 
+import java.lang.reflect.Constructor;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.varia.NullAppender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import asu.edu.neg_rule_miner.RuleMinerException;
+import asu.edu.neg_rule_miner.sparql.SparqlExecutor;
 
 public class ConfigurationFacility {
 
 	private static Configuration instance;
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(ConfigurationFacility.class.getName());
 
 	public synchronized static Configuration getConfiguration(){
+		
 
 		if(instance == null){
 			BasicConfigurator.configure(new NullAppender());
@@ -34,6 +41,32 @@ public class ConfigurationFacility {
 		}
 		return instance;
 
+	}
+	
+	public SparqlExecutor getSparqlExecutor(){
+
+		if(!ConfigurationFacility.getConfiguration().containsKey(Constant.CONF_SPARQL_ENGINE))
+			throw new RuleMinerException("Sparql engine parameters not found in the configuration file.", 
+					LOGGER);
+
+		Configuration subConf = ConfigurationFacility.getConfiguration().subset(Constant.CONF_SPARQL_ENGINE);
+
+		if(!subConf.containsKey("class"))
+			throw new RuleMinerException("Need to specify the class implementing the Sparql engine "
+					+ "in the configuration file under parameter 'class'.", LOGGER);
+
+		SparqlExecutor endpoint;
+		try{
+			Constructor<?> c = Class.forName(subConf.getString("class")).
+					getDeclaredConstructor(Configuration.class);
+			c.setAccessible(true);
+			endpoint = (SparqlExecutor) 
+					c.newInstance(new Object[] {subConf});
+		}
+		catch(Exception e){
+			throw new RuleMinerException("Error while instantiang the sparql executor enginge.", e,LOGGER);
+		}
+		return endpoint;
 	}
 
 }
